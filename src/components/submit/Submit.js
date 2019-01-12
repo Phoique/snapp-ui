8d2bc0414302a738f32ca003b8b65cc82684a498
Phoique <official.phoique7@yandex.com>
2019-01-13 01:40:03 +0300

    Transferring the value entered state

--- src/components/submit/Submit.js
+++ src/components/submit/Submit.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import './Submit.css';
 
 class Submit extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.sendMessage = this.sendMessage.bind(this);
+
+    this.state = {
+      writeMessage: ''
+    }
+
+  }
+
+  sendMessage(event) {
+    this.setState({writeMessage: event.target.value});
+  }
+
   render() {
     return(
       <div className="gonderici">
-        <input className="yolla" type="text"/>
+      <form>
+        <input className="yolla" type="text" onChange={this.sendMessage}/>
         <input type="submit" value="Gönder" className="gonderbuton" />
+      </form>
       </div>
     );
   }
